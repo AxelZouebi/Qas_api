e62b4f9b6276a5aff57485081d693b9aba83c330
Axel <72017288+AxelZouebi@users.noreply.github.com>
2021-10-01 13:25:59 +0200

    Adding Affluence route

--- routes.py
+++ routes.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from bson.json_util import dumps
+import pymongo
+import certifi
 
 app = Flask(__name__)
 
@@ -8,7 +11,16 @@ def display():
 
 @app.route("/affluence")
 def display_affluence():
-    return 'affluence'
+    ca = certifi.where()
+    client=pymongo.MongoClient('mongodb+srv://dbSmartcy:Dsrush2021@cluster0.linaa.mongodb.net/Smartcy', tlsCAFile=ca)
+    db = client['Smartcy']
+    affluence = db.affluence
+    json_lieu = list(affluence.find())
+    All_lieu = []
+    for key in json_lieu:
+        All_lieu.append(key)
+    json_data = dumps(All_lieu)
+    return json_data
 
 @app.route("/lieu")
 def display_lieu():
