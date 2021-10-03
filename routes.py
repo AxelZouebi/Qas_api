1945b6101248a2b55ff3e88a9e4def723314570a
Axel <72017288+AxelZouebi@users.noreply.github.com>
2021-10-03 18:38:39 +0200

    Adding affluence route get (as before) & post_affluence route (succed one)

--- routes.py
+++ routes.py
@@ -12,63 +12,37 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+ca = certifi.where()
+client=pymongo.MongoClient('mongodb+srv://dbSmartcy:Dsrush2021@cluster0.linaa.mongodb.net/Smartcy', tlsCAFile=ca)
+db = client['Smartcy']
+affluence = db.affluence
+
 @app.route("/")
 @cross_origin()
 def display():
     return 'root'
 
-# @app.route("/affluence", methods = ['GET'])
-# @cross_origin()
-# def display_affluence():
-#     ca = certifi.where()
-#     client=pymongo.MongoClient('mongodb+srv://dbSmartcy:Dsrush2021@cluster0.linaa.mongodb.net/Smartcy', tlsCAFile=ca)
-#     db = client['Smartcy']
-#     affluence = db.affluence
-#     json_lieu = list(affluence.find())
-#     All_lieu = []
-#     for key in json_lieu:
-#         All_lieu.append(key)
-#     json_data = dumps(All_lieu)
-#     return json_data
-
-# @app.route("/affluence.json", methods = ['POST'])
-# @cross_origin()
-# def post_affluence():
-#     ca = certifi.where()
-#     client=pymongo.MongoClient('mongodb+srv://dbSmartcy:Dsrush2021@cluster0.linaa.mongodb.net/Smartcy', tlsCAFile=ca)
-#     db = client['Smartcy']
-#     if request.method == 'POST':
-#         print('post method la ici')
-#         data = request.get_json()
-#         address = data['address']
-#         url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) +'?format=json'
-#         response = requests.get(url).json()
-#         lat = response[0]["lat"]
-#         lon = response[0]["lon"]
-#         affluence_json = {'lat':lat, 'lon':lon, 'densite':data['densite'], 'remarque':data['remarque']}
-#         affluence = db.affluence
-#         affluence.insert_one(affluence_json)
-#         return jsonify(affluence_json)
-
-# @app.route("/lieu")
-# @cross_origin()
-# def display_lieu():
-#     return 'lieu'
-
-
-ca = certifi.where()
-client=pymongo.MongoClient('mongodb+srv://dbSmartcy:Dsrush2021@cluster0.linaa.mongodb.net/Smartcy', tlsCAFile=ca)
-db = client['Smartcy']
-affluence = db.affluence
-@app.route('/postjson', methods = ['POST'])
+@app.route("/affluence", methods = ['GET'])
 @cross_origin()
-def postJsonHandler():
+def display_affluence():
+    json_lieu = list(affluence.find())
+    All_lieu = []
+    for key in json_lieu:
+        All_lieu.append(key)
+    json_data = dumps(All_lieu)
+    return json_data
+
+
+
+@app.route('/post_affluence', methods = ['POST'])
+@cross_origin()
+def post_affluence():
     if request.is_json:
         content = request.get_json()
         affluence.insert_one(content)
-        return {"sucess":"True"}
+        return {"sucsess":"True"}
     else:
-        return {"miiiskine": "ta le seum"}
+        return {"success": "But not json"}
 
 if __name__ == "__main__":
     app.run()
